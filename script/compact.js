64a352e54776af69d630aadfa467ea44aac40e1d
Rudolf Schmidt <me@rudionrails.com>
2012-06-27 16:59:48 +0200

    FIX: Solvd problem when trying to compact another database when returning from recovery

--- script/compact.js
+++ script/compact.js
@@ -1,12 +1,16 @@
-/**
+/*!
+ * MongoDB Backup Script
  *
- * MongoDB maintenance script to compact all collections in all databases.
- *
- * Copyright (c) 2012, Rudolf Schmidt
+ * Copyright 2012, Rudolf Schmidt
  * Released under the MIT license.
  *
+ * More information: http://github.com/rudionrails/mongodb-maintenance
  */
 
+
+/**
+ * Misc function to pretty print messages to stdout
+ */
 function say( m ) {
   var date = new Date(),
       timestamp = date.toLocaleDateString() +" "+ date.toLocaleTimeString();
@@ -14,10 +18,19 @@ function say( m ) {
   print( timestamp +": "+ m );
 }
 
+/**
+ * The following variables are used to exclude databases/collections from being compacted.
+ */
+var dbRegex = /^(admin|local|test)/,
+    colRegex = /^(system)/;
+
+/**
+ * The actual execution begins here.
+ */
 try {
 
   /**
-   * check if we are the master and step down if necessary
+   * perform some pre-checks
    */
   if ( rs.status().ok == 0 ) {
     say( "[WARN] There seems to be no replica set configured. Continuing anyways." );
@@ -29,16 +42,24 @@ try {
     if ( rs.isMaster().ismaster ) {
       say( "[INFO] Connected to PRIMARY, going to step down..." );
 
+      /**
+       * perform primary step-down
+       */
       try {
         rs.stepDown();
       } catch( e ) {
         // do nothing
       }
 
-      // After stepdown, connections are droped. Reconnect.
+      /**
+       * After stepdown, connections are droped. Issue a command to reconnect.
+       */
       rs.isMaster();
 
-      // Let's wait for someone else to become the new PRIMARY
+      /**
+       * It can take several seconds to promote a new primary. In some cases it
+       * may never happen, then we exit.
+       */
       var count = 1;
       while( rs.isMaster().ismaster || !rs.isMaster().primary ) {
         if ( count < 20 ) {
@@ -68,14 +89,21 @@ try {
    * Compact from here onwards
    */
   say( "[INFO] Performing compact" );
-  var dbNames = db.getMongo().getDBNames(),
-      dbRegex = /^(admin|local|test)/,
-      colRegex = /^(system)/;
+
+
+  /**
+   * Iterate all databases
+   */
+  var dbNames = db.getMongo().getDBNames();
 
   for( var i=0; i<dbNames.length; i++ ) {
-    var dbName = dbNames[i],
-        dbInst = db.getSisterDB(dbName);
+    var dbName    = dbNames[i],
+        dbInst    = db.getSisterDB(dbName),
+        colNames  = dbInst.getCollectionNames();
 
+    /**
+     * Check if we want to skip the database or continue.
+     */
     if( dbRegex.test(dbName) ) {
       say( dbName +" (skipped)" );
       continue; // skip
@@ -85,34 +113,57 @@ try {
 
     }
 
-    // Iterate all collections on the current db and compact
-    try {
-      var colNames = dbInst.getCollectionNames();
+    /**
+     * Iterate all collections on the current db and compact
+     */
+    for( var j=0; j<colNames.length; j++ ) {
+      var colName = colNames[j],
+          colInst = dbInst.getCollection(colName);
 
-      for( var j=0; j<colNames.length; j++ ) {
-        var colName = colNames[j],
-            colInst = dbInst.getCollection(colName);
+      /**
+       * Check if we want to skip the collection or continue.
+       */
+      if( colRegex.test(colName) ) {
+        say( "\t- "+ colName +" (skipped)" );
+        continue; // skip
 
-        if( colRegex.test(colName) ) {
-          say( "\t- "+ colName +" (skipped)" );
-          continue; // skip
+      } else {
+        say( "\t- "+ colName );
 
-        } else {
-          say( "\t- "+ colName );
-
-        }
-
-        try {
-          dbInst.runCommand({ compact : colName });
-
-        } catch( e ) {
-          print( "[ERROR] "+ e );
-
-        }
       }
-    } catch( e ) {
-      say( "[ERROR] "+ e )
 
+      /**
+       * Perform compaction on current collection
+       */
+      try {
+        var compact = dbInst.runCommand({ compact : colName });
+
+        // something went wrong
+        if ( compact.ok == 0 ) {
+          throw "Compaction of "+ colName +" resulted in an error: "+ compact.toSource()
+
+        }
+
+        /**
+         * After compaction, the node goes into recovery, so we need to wait for 
+         * it to return to normal.
+         */
+        var count = 1;
+        while( !rs.isMaster().secondary ) {
+          if ( count < 20 ) {
+            sleep( 1000 );
+            count++;
+
+          } else {
+            throw "Timed out waiting to return from recovery after "+ count +" tries"
+
+          }
+        }
+
+      } catch( e ) {
+        say( "[ERROR] "+ e );
+
+      }
     }
 
   }
